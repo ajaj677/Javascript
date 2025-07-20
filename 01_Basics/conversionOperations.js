a96ebcba6cdb8e1019328021f4500318d590a96c
MD AJAJ <ajajm677@gmail.com>
2025-07-20 18:12:39 +0000

    container config done

--- 01_Basics/conversionOperations.js
+++ 01_Basics/conversionOperations.js
@@ -22,9 +22,32 @@ console.log(booleanIsLoggedIn) // => true
 let someNumber = 33
 let stringNumber = String(someNumber)
 
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
 
 /************************ Operations ************************************************** */
 let value = 3
 let negValue = -value
-console.log(negValue)
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/6);
+// console.log(2%3);
+
+ let str1 ="hello"
+ let str2 = " world"
+ let str3 = str1+str2
+//  console.log(str3);
+
+//  console.log("1"+2+2);
+
+//  console.log(+true); // output => 1
+
+ 
+ let gameCounter =100
+ gameCounter++;
+ console.log(gameCounter);
+ 
+ 
+ 

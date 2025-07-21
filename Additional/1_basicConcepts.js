354bec98c2b1622e57140f125289635edd9c62eb
MD AJAJ <ajajm677@gmail.com>
2025-07-21 19:35:30 +0000

    pending

---
+++ Additional/1_basicConcepts.js
@@ -0,0 +1,74 @@
+//Assigment operators
+
+// let a = 5
+// let b = 2
+
+// a += 4
+// console.log(a); //6
+
+// a -= 1
+// console.log(a); //1
+
+// a % 4
+// console.log(a); //5
+
+//__________Comparison operators__________//
+
+// if(a==b){
+//     console.log("True");
+// } else{
+//     console.log("false"); //false
+// }
+
+// if(a!=b){
+//     console.log("True");
+// } else{
+//     console.log("false"); //true
+// }
+
+// "5" == 5 -> true javscript convert string to number
+
+// === -> strict check, check datatypes and values 
+
+// if(a===b){
+//     console.log("True");
+// } else{
+//     console.log("false"); //false
+// }
+
+// if(a>b){
+//     console.log("True");
+// } else{
+//     console.log("false"); //true
+// }
+
+console.log('_____ Logical operators _____');
+
+// logical AND &&;  true|true -> true
+// or ||    false|true -> true
+// not !
+// Output -> boolean
+
+let a = 6
+let b = 5
+
+let cond1 = a>b //true
+let cond2 = a===8 //false
+
+if (cond1 || OffscreenCanvasRenderingContext2D){
+    console.log("true");
+} else{
+    console.log("false");
+}
+
+for (let i = 1; i < 5; i++){
+    console.log(`Counting: ${i}`);
+}
+
+function add(x,y){
+    return x+y;
+}
+
+console.log(`Result: ${add(5,2)}`);
+
+

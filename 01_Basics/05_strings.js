0b2c6f96cbfa81cdb2403e90ba52a2f2901f8477
MD AJAJ <ajajm677@gmail.com>
2025-07-21 15:17:19 +0000

    string comppleted

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -17,8 +17,32 @@ console.log(gameName.indexOf("j")); //tells us the index of that character
 const newString = gameName.substring(0, 2)
 console.log(newString);
 
-const anotherString = gameName.slice(-3,2)
-console.log(anotherString);
+const anotherStrings = gameName.slice(-3,2)
+console.log(anotherStrings);
+
+console.log("**************");
+
+const newStringOne = "      ajaj "
+console.log(newStringOne.trim()); // trim start and trim end, are also there works on whitespace
+
+//replace the things
+const url = "https://ajaj.com/ajaj%20mdn"
+console.log(url.replace("%20", "-"));
+
+
+//find if the words include in that string 
+console.log(url.includes("ajaj"));
+
+//converts a string into array based on given values like spaces, commas, etc
+// seperator, limit
+console.log(gameName.split("j"));
+
+//refer string javascript mdn for more string knowledge.
+
+
+
+
+
 
 
 
